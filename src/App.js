1b07dbcf4bb3225f21cb8f06e25e95e81da11fb9
Raman.Naumovich <naumovichhh@gmail.com>
2019-08-22 13:05:41 +0300

    Bootstrap added. Login now can redirect if login&password matches.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Login from './Login';
 import Home from './Home';
+import Admin from './Admin';
 
 function App() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path={["/", "/home"]} component={Home} />
-                <Route exact path="/login" component={Login} />
+                <Route exact path="/home" component={Home} />
+                <Route exact path="/login" component={Login} />} />
+                <Route exact path="/admin" component={Admin} />
+                <Redirect exact from="/" to="home" />
                 <Route component={NoMatch} />
             </Switch>
         </BrowserRouter>
--- src/Login.js
+++ src/Login.js
@@ -1,25 +1,26 @@
 import React from 'react';
+import accounts from './accounts.json';
 
 class Login extends React.Component {
     constructor(properties) {
         super(properties);
-        this.state = { user: "", password: "", userIsValid: true, passwordIsValid: true };
+        this.state = { login: "", password: "", loginIsValid: true, passwordIsValid: true };
     }
 
     passwordIsInvalidMessage = "Password must contain at least 8 characters";
-    userIsInvalidMessage = "User must contain valid e-mail address";
+    loginIsInvalidMessage = "User must contain valid e-mail address";
 
     onUserChange = (e) => {
-        this.setState({ user: e.target.value });
+        this.setState({ login: e.target.value });
     }
 
     onPasswordChange = (e) => {
         this.setState({ password: e.target.value });
     }
 
-    checkUserValid(s) {
+    checkLoginValid(s) {
         if (s.search(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) != -1)
-            return this.setState;
+            return true;
         else
             return false;
     }
@@ -33,29 +34,48 @@ class Login extends React.Component {
 
     onSubmit = (e) => {        
         e.preventDefault();
-        let userIsValid = this.checkUserValid(this.state.user);
+        let loginIsValid = this.checkLoginValid(this.state.login);
         let passwordIsValid = this.checkPasswordValid(this.state.password);
-        if (userIsValid & passwordIsValid) {
-            //do authorize
+        if (loginIsValid & passwordIsValid) {
+            this.logIn();
         }
         else {
-            this.setState({ userIsValid: userIsValid, passwordIsValid: passwordIsValid });
+            this.setState({ loginIsValid: loginIsValid, passwordIsValid: passwordIsValid });
+        }
+    }
+
+    logIn() {
+        let login = this.state.login, password = this.state.password;
+        if (accounts.admins.find(e => e.login === login && e.password === password) !== undefined) {
+            window.open('/admin', '_self');
+        }
+        else {
+            if (accounts.users.find(e => e.login === login && e.password === password) !== undefined) {
+                window.open('/home', '_self');
+            }
+            else {
+                this.setState({ wrongData: true });
+            }
         }
     }
 
     render() {
         return (
-            <form>
-                <div>
-                    <input name="user" type="text" value={this.state.user} onChange={this.onUserChange} />
-                    {!this.state.userIsValid && <span style={{color: "red"}} >{this.userIsInvalidMessage}</span>}
+            <form style={{marginLeft: "20px", marginTop: "20px"}} role="form" className="form form-horizontal" >
+                <div className="form-group">
+                    <div className="col-md-2 col-sm-4">
+                        <input className="form-control" placeholder="Username" name="user" type="text" value={this.state.login} onChange={this.onUserChange} />
+                    </div>
+                    {!this.state.loginIsValid && <span className="col-md-2 col-sm-4" style={{color: "red"}} >{this.loginIsInvalidMessage}</span>}
                 </div>
-                <div>
-                    <input name="password" type="password" value={this.state.password} onChange={this.onPasswordChange}/>
-                    {!this.state.passwordIsValid && <span style={{color: "red"}}>{this.passwordIsInvalidMessage}</span>}
+                <div className="form-group">
+                    <div className="col-md-2 col-sm-4">
+                        <input className="form-control" placeholder="Password" name="password" type="password" value={this.state.password} onChange={this.onPasswordChange}/>
+                    </div>
+                    {!this.state.passwordIsValid && <span className="col-md-2 col-sm-4" style={{color: "red"}}>{this.passwordIsInvalidMessage}</span>}
                 </div>
-                <div>
-                    <input name="send" type="submit" value="Войти" onClick={this.onSubmit} />
+                <div className="form-group col-md-2 col-sm-4">
+                    <input className="btn btn-primary" name="send" type="submit" value="Войти" onClick={this.onSubmit} />
                 </div>
             </form>
         );
