e8c457d3cf7454b61b6b295738ce03eb1985107f
Raman.Naumovich <naumovichhh@gmail.com>
2020-01-03 14:18:35 +0300

    Repository interfaces added

---
+++ Api/Core/Repositories/IRoomsRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities;
+
+namespace Core.Repositories
+{
+    public interface IRoomsRepository
+    {
+        RoomEntity Get(int id);
+
+        IEnumerable<RoomEntity> Get();
+
+        RoomEntity Create(RoomEntity room);
+
+        RoomEntity Update(RoomEntity room);
+
+        RoomEntity Delete(int id);
+    }
+}
